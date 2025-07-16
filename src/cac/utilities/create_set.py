745a49ce02c80f40887fdd8b3081cb0f09119848
Ryan Santoso <r.santoso@fz-juelich.de>
2025-07-16 15:27:35 +0200

    Update create_set.py
    Enabling process-based machine learning

--- src/cac/utilities/create_set.py
+++ src/cac/utilities/create_set.py
@@ -93,3 +93,80 @@ def create_set(pool_image_array=None, pool_label_array=None,
         df.to_csv(documentation_path + '/training_test_prediction_splits.csv',index=False)
 
         return training_images, training_labels, test_images, test_labels, prediction_images, prediction_labels
+
+
+#Function to create training and test set for nirb
+def create_set_nirb(training_input_array=None, training_output_array=None,
+                    test_input_array=None, test_output_array=None,
+                    save_path=None):
+    
+    #Scaling inputs
+    inputs_scaler = preprocessing.StandardScaler().fit(training_input_array)
+    scaled_training_input_array = inputs_scaler.transform(training_input_array)
+    scaled_test_input_array     = inputs_scaler.transform(test_input_array)
+
+    scaling_factor = []
+    scaling_factor.append(inputs_scaler.mean_)
+    scaling_factor.append(inputs_scaler.var_)
+    scaling_factor = numpy.asarray(scaling_factor)
+    
+    numpy.save(save_path + "/inputs_standard_scaling_factor", scaling_factor)
+
+
+    #Scaling outputs
+    if(training_output_array.ndim < 3):
+        outputs_scaler = preprocessing.MinMaxScaler().fit(training_output_array)
+        scaled_training_output_array = outputs_scaler.transform(training_output_array)
+        scaled_test_output_array     = outputs_scaler.transform(test_output_array)
+        
+        scaling_factor = []
+        scaling_factor.append(outputs_scaler.data_min_)
+        scaling_factor.append(outputs_scaler.data_max_)
+        scaling_factor = numpy.asarray(scaling_factor)
+
+        numpy.save(save_path + "/outputs_minmax_scaling_factor", scaling_factor)
+        
+        #Return set
+        return scaled_training_input_array, scaled_training_output_array, scaled_test_input_array, scaled_test_output_array 
+    else:
+        Ytrain = []
+        for k in range(training_output_array.shape[1]):
+            for j in range(training_output_array.shape[0]):
+                Ytrain.append(training_output_array[j, k, :])
+        Ytrain = numpy.asarray(Ytrain) 
+
+        Ytest = []
+        for k in range(test_output_array.shape[1]):
+            for j in range(test_output_array.shape[0]):
+                Ytest.append(test_output_array[j, k, :])
+        Ytest = numpy.asarray(Ytest) 
+        
+        outputs_scaler = preprocessing.MinMaxScaler().fit(Ytrain)
+        scaled_Ytrain  = outputs_scaler.transform(Ytrain)
+        scaled_Ytest   = outputs_scaler.transform(Ytest)
+        
+        scaled_training_output_array = numpy.zeros((training_output_array.shape[0], training_output_array.shape[1], training_output_array.shape[2])) 
+        scaled_test_output_array     = numpy.zeros((test_output_array.shape[0], test_output_array.shape[1], test_output_array.shape[2])) 
+        
+        aa = 0
+        for k in range(training_output_array.shape[1]):
+            for j in range(training_output_array.shape[0]):
+                scaled_training_output_array[j, k, :] = scaled_Ytrain[aa,:]
+                aa = aa + 1 
+
+        aa = 0
+        for k in range(test_output_array.shape[1]):
+            for j in range(test_output_array.shape[0]):
+                scaled_test_output_array[j, k, :] = scaled_Ytest[aa,:]
+                aa = aa + 1 
+
+        scaling_factor = []
+        scaling_factor.append(outputs_scaler.data_min_)
+        scaling_factor.append(outputs_scaler.data_max_)
+        scaling_factor = numpy.asarray(scaling_factor)
+
+        numpy.save(save_path + "/outputs_minmax_scaling_factor", scaling_factor)
+
+        #Return set
+        return scaled_training_input_array, scaled_training_output_array, scaled_Ytrain, \
+               scaled_test_input_array, scaled_test_output_array, scaled_Ytest 
