5b6596b3dd36a039ab72564e4ae48362d702769c
Ryan Santoso <r.santoso@fz-juelich.de>
2025-07-16 15:19:00 +0200

    Update __init__.py
    Enabling process-based machine learning

--- src/cac/model/__init__.py
+++ src/cac/model/__init__.py
@@ -1,6 +1,12 @@
 #-----------------------------------------------------
 #Import internal libraries
 from .create_unet import *
+from .create_cae2D import *
+from .create_nirb import *
+from .create_nirb_gpr import *
+from .create_nirb_resnet import *
+from .create_nirb_bt import *
+from .online_nirb import *
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
