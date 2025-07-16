ef4ec0b38612a91aa39b9532ed4ea8ec294a408b
Ryan Santoso <r.santoso@fz-juelich.de>
2025-07-16 15:23:18 +0200

    Update __init__.py
    Enabling process-based machine learning

--- src/cac/trainer/__init__.py
+++ src/cac/trainer/__init__.py
@@ -1,6 +1,7 @@
 #-----------------------------------------------------
 #Import internal libraries
 from .train import *
+from .nirb import *
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import warnings
