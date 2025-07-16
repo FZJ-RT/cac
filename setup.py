60fc8e0e1be66ea081c9eaba65e61f748f5a5111
Ryan Santoso <r.santoso@fz-juelich.de>
2025-07-16 15:29:31 +0200

    Update setup.py
    Adding process-based machine learning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cac',
-    version='1.0',
+    version='2.0',
     description='A library for analyzing chip properties',
     packages=['cac', 'cac.model','cac.trainer','cac.utilities'],
     package_dir={'': 'src'},
