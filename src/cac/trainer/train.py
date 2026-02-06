5191fb424f6e7f3f411a926a9602459e1f615992
Ryan Santoso <r.santoso@fz-juelich.de>
2026-02-06 13:48:23 +0100

    Update train.py

--- src/cac/trainer/train.py
+++ src/cac/trainer/train.py
@@ -417,3 +417,136 @@ def train_regression_without_generator_bt(training_images, training_labels,
                       verbose=0)
 
     return model
+
+
+#Train tfunet
+def train_tfunet(training_images, modification_value, training_labels, 
+                 model, batch_size, epochs,
+                 distributed=False,
+                 save_path=None):
+    
+    #Initialize generator
+    steps_per_epoch_tr = len(numpy.array_split(numpy.zeros(len(training_images)), int(len(training_images)/batch_size)))
+
+    #Training
+    if(save_path!=None):
+
+        if(distributed==True):
+            #Record training
+            csvlog = CSVLogger(save_path + '/training_log.csv')
+            stopping = keras.callbacks.EarlyStopping(monitor='loss',
+                                                     restore_best_weights=True)
+            callbacks = [
+                    hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+                    hvd.callbacks.MetricAverageCallback(),
+                    csvlog,
+                    stopping]
+
+            #Fitting
+            model.fit([training_images, modification_value], training_labels,
+                    steps_per_epoch=steps_per_epoch_tr // hvd.size(),
+                    epochs=epochs,
+                    verbose=0,
+                    callbacks=callbacks)
+        
+            #Saving
+            strr = save_path + "/trained_model.tf"
+            save_model(model, strr)
+        else:
+            #Record training
+            csvlog = CSVLogger(save_path + '/training_log.csv')
+            stopping = keras.callbacks.EarlyStopping(monitor='loss',
+                                            restore_best_weights=True)
+            #Fitting
+            model.fit([training_images, modification_value], training_labels,
+                      batch_size=batch_size,
+                      epochs=epochs,
+                      verbose=0,
+                      callbacks=[csvlog, stopping])
+
+            #Saving
+            strr = save_path + "/trained_model.tf"
+            save_model(model, strr)
+
+    else:
+        
+        if(distributed==True):
+            #Record training
+            stopping = keras.callbacks.EarlyStopping(monitor='loss',
+                                            restore_best_weights=True)
+            callbacks = [
+                    hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+                    hvd.callbacks.MetricAverageCallback(),
+                    stopping]
+
+            #Fitting
+            model.fit([training_images, modification_value], training_labels,
+                      steps_per_epoch=steps_per_epoch_tr // hvd.size(),
+                      epochs=epochs,
+                      verbose=0,
+                      callbacks=callbacks)
+        else:
+            #Fitting
+            stopping = keras.callbacks.EarlyStopping(monitor='loss',
+                                            restore_best_weights=True)
+            model.fit([training_images, modification_value], training_labels,
+                      batch_size=batch_size,
+                      epochs=epochs,
+                      verbose=0,
+                      callbacks=[stopping])
+
+    return model
+
+
+#Function to calculate error prediction in image
+def calculate_image_metric_tfunet(test_images, modification_value, test_labels, 
+                                  model, 
+                                  save_path=None):
+
+    #Using model to make prediction on test set
+    p_test_labels = model.predict([test_images, modification_value], batch_size=1)
+
+    #Thresholding
+    p_test_labels[p_test_labels < 0.5]  = 0
+    p_test_labels[p_test_labels >= 0.5] = 1
+
+    #Transfering to 2-D matrix
+    true_matrix      = numpy.zeros((test_labels.shape[0], test_labels.shape[1] * test_labels.shape[2] * 1))
+    predicted_matrix = numpy.zeros((p_test_labels.shape[0], p_test_labels.shape[1] * p_test_labels.shape[2] * 1))
+    for ww in range(test_labels.shape[0]):
+        true_matrix[ww, :] = test_labels[ww, :, :, :].flatten()
+        predicted_matrix[ww, :] = p_test_labels[ww, :, :, :].flatten()
+    
+    #Error calculation
+    test_error = numpy.mean( (true_matrix - predicted_matrix)**2 )/numpy.mean( true_matrix**2 )
+
+    #Saving options
+    if(save_path!=None):
+        numpy.savetxt(save_path + '/test_error.txt', [test_error])
+
+    #Return error
+    return test_error
+
+
+#Function to calculate jacard index
+def calculate_jaccard_score_tfunet(test_images, modification_value, test_labels, 
+                                   model,
+                                   save_path=None):
+
+    #Using model to make prediction on test set
+    p_test_labels = model.predict([test_images, modification_value], batch_size=1)
+
+    #Thresholding
+    p_test_labels[p_test_labels < 0.5]  = 0
+    p_test_labels[p_test_labels >= 0.5] = 1
+
+    #Transfering to 2-D matrix
+    jaccard = []
+    for ww in range(test_labels.shape[0]):
+        jaccard.extend([jaccard_score(test_labels[ww, :, :, :].flatten(), p_test_labels[ww, :, :, :].flatten())])
+
+    #Saving options
+    if(save_path!=None):
+        numpy.savetxt(save_path + '/jaccard.txt', jaccard)
+
+    #Return error
