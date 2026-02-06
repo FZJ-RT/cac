43788262fd9e4eaba5979f9e70cd15b8cdb85da7
Ryan Santoso <r.santoso@fz-juelich.de>
2026-02-06 13:47:34 +0100

    Update __init__.py

--- src/cac/model/__init__.py
+++ src/cac/model/__init__.py
@@ -7,6 +7,7 @@ from .create_nirb_gpr import *
 from .create_nirb_resnet import *
 from .create_nirb_bt import *
 from .online_nirb import *
+from .create_tfunet import *
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
